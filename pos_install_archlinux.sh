3aef2ea4873df26b101dddcd105a3daf14fe109b
Hugo Torres <hugotorres@protonmail.ch>
2018-08-28 14:50:13 -0300

    Feito ajustes

--- pos_install_archlinux.sh
+++ pos_install_archlinux.sh
@@ -6,36 +6,38 @@ echo "[+] Mudando fuso horário para America/São Paulo."
 ln -sf /usr/share/zoneinfo/America/Sao_Paulo /etc/localtime
 hwclock --systohc --utc
 
-# Configurando idioma e teclado
+# Configurando idioma do sistema
 echo "[+] Configurando idioma para Inglês"
 echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
 locale-gen
 echo "LANG=en_US.UTF-8" >> /etc/locale.conf
 
-echo "Configurando teclado para ABNT2"
+# Configurando teclado
 echo "KEYMAP=br-abnt2" >> /etc/vconsole.conf
-# Configurar o teclado para o ambiente X
 localectl set-x11-keymap br abnt2
 
 # ================================================
 # Instalação de programas 
 # ================================================
-echo "[+] Instalando programas."
 # Básicos
-pacman -S --noconfirm sudo intel-ucode zsh xf86-input-synaptics xorg xorg-xinit fluxbox lxdm xterm ntfs-3g ranger epdfview scrot cron
+pacman -S --noconfirm sudo intel-ucode zsh xf86-input-synaptics xorg xorg-xinit fluxbox lxdm xterm ntfs-3g ranger htop epdfview scrot cron xdg-user-dirs
 
 # Rede
 pacman -S --noconfirm wireless_tools wpa_actiond dialog network-manager-applet links
 
 # Áudio/Video
-pacman -S --noconfirm pavucontrol alsa-firmware alsa-utils alsa-plugins pulseaudio-alsa pulseaudio moc mpv vlc 
+pacman -S --noconfirm pavucontrol alsa-firmware alsa-utils alsa-plugins pulseaudio-alsa pulseaudio moc mpv vlc volmeicon
+
+# Imagem
+pacman -S --noconfirm nitrogen gimp inkscape
 
 # Configurando o sudo
-echo "[+] Abilitando o grupo Wheel para usar o SUDO."
-sed -i 's/# %wheel ALL=(ALL) ALL/%wheel ALL=(ALL) ALL/' >> /etc/sudoes
+sed -i 's/# %wheel ALL=(ALL) ALL/%wheel ALL=(ALL) ALL/' >> /etc/sudoers
 
-# Abilitando gerenciador de exibição
-echo "Abilitando o gerenciador de login LXDM"
+# Criando diretórios do usuário.
+xdg-user-dirs-update
+
+# Abilitando gerenciador de login.
 systemctl enable lxdm.service
 
 reboot
