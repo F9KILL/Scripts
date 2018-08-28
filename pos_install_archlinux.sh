f6894eaec20f1c7e6ff459e1353ac472598b515c
Hugo Torres <hugotorres@protonmail.ch>
2018-08-28 20:05:25 -0300

    Configuração do modo de suspenção

--- pos_install_archlinux.sh
+++ pos_install_archlinux.sh
@@ -2,12 +2,10 @@
 # Script para fazer a configuração e instalação de programas, depois da instalação do archlinux.
 
 # configurando horário
-echo "[+] Mudando fuso horário para America/São Paulo."
 ln -sf /usr/share/zoneinfo/America/Sao_Paulo /etc/localtime
 hwclock --systohc --utc
 
 # Configurando idioma do sistema
-echo "[+] Configurando idioma para Inglês"
 echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
 locale-gen
 echo "LANG=en_US.UTF-8" >> /etc/locale.conf
@@ -36,6 +34,10 @@ sed -i 's/# %wheel ALL=(ALL) ALL/%wheel ALL=(ALL) ALL/' >> /etc/sudoers
 
 # Criando diretórios do usuário.
 xdg-user-dirs-update
+
+# Configuração modo de suspensão
+echo "HandleLidSwitch=suspend" >> /etc/systemd/logind.conf
+echo "HandleLidSwitchDocked=suspend" >> /etc/systemd/logind.conf
 
 # Abilitando gerenciador de login.
 systemctl enable lxdm.service
